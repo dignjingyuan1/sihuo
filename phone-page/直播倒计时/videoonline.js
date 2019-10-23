b914c026c7c3e75e7c2ed8089aceb3a7ec92b2c8
DJY <534215871@qq.com>
2019-10-23 13:53:40 +0800

    1.企业展示-搜索结果 2.企业战士详情页 3.企业展示首页 4.直播倒计时 5.分类下拉菜单 这些是新做的页面 分类下拉菜单做在课程首页里面了

---
+++ phone-page/直播倒计时/videoonline.js
@@ -0,0 +1,27 @@
+/**
+ * 播放视频
+ */
+function playVideo(){
+    document.getElementById('videoImg').style.display = "none";
+    document.getElementById('videoButton').style.display = "none";
+    document.getElementById('videoPlay').style.display = "inline-block";
+    document.getElementById('videoPlay').play();
+}
+
+
+/**
+ * 倒计时
+ */
+function timeOut(date) {
+    var bb = setInterval(function () {
+        date -= 1000;
+        var day = Math.floor(date / 86400000);
+        var hour = Math.floor((date-day*86400000) / 3600000)
+        var minu = Math.floor(((date-day*86400000) - hour * 3600000) / 60000)
+        var score = Math.floor((((date-day*86400000) - hour * 3600000) - minu*60000) / 1000)
+        document.getElementById('timeOut').innerText = day + "天" + hour + '小时' + minu + '分钟' + score + '秒'
+    }, 1000)
+}
+
+var date = document.getElementById('timeOutDate').value * 1 - new Date().getTime()
+timeOut(date)
